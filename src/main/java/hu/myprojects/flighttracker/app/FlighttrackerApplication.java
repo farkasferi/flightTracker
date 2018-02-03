20012eb4c4e25f1b1985c0710887f63209a4b97c
farkasferi <frantyo3333@gmail.com>
2018-02-03 10:14:48 +0100

    Initial commit with spring initializr

---
+++ src/main/java/hu/myprojects/flighttracker/app/FlighttrackerApplication.java
@@ -0,0 +1,12 @@
+package hu.myprojects.flighttracker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlighttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlighttrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/hu/myprojects/flighttracker/FlighttrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.myprojects.flighttracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlighttrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
