12376ce1cca0d2540a47c84a594ef6bdb5d1d83a
farkasferi <frantyo3333@gmail.com>
2018-02-04 13:40:10 +0100

    Added templates for update and delete flight.

--- src/main/java/hu/myprojects/flighttracker/controller/FlightController.java
+++ src/main/java/hu/myprojects/flighttracker/controller/FlightController.java
@@ -24,41 +24,54 @@ public class FlightController {
     private FlightRepository repository;
 
     @RequestMapping(value = "/flight/insert", method = RequestMethod.POST)
-    public String insertFlight(
-            @Valid @ModelAttribute Flight flight, BindingResult errors, Model model){
+    public String insertFlight( @Valid @ModelAttribute Flight flight, BindingResult errors, Model model) {
 
         if (!errors.hasErrors()) {
             flightService.insertFlight(flight);
             List<Flight> flights = repository.findAll();
             model.addAttribute("flights",flights);
         }
-        return ((errors.hasErrors()) ? "addFlight.html" : "listFlight.html");
+        return ((errors.hasErrors()) ? "insertFlight.html" : "listFlight.html");
     }
 
-    @RequestMapping(value = "/flight/add", method = RequestMethod.GET)
-    public String addFlight(
-            Model model){
+    @RequestMapping(value = "/flight/insert", method = RequestMethod.GET)
+    public String insertFlight(Model model) {
 
         model.addAttribute("flight", new Flight());
-        return "addFlight.html";
+        return "insertFlight.html";
     }
 
     @RequestMapping(value = "/flight/update", method = RequestMethod.POST)
-    public void updateFlight(
-            @RequestBody Flight flight) {
+    public String updateFlight( @Valid @ModelAttribute Flight flight, BindingResult errors, Model model) {
 
-        flightService.updateFlight(flight);
+        if (!errors.hasErrors()) {
+            flightService.updateFlight(flight);
+            List<Flight> flights = repository.findAll();
+            model.addAttribute("flights",flights);
+        }
+        return ((errors.hasErrors()) ? "updateFlight.html" : "listFlight.html");
     }
 
-    @RequestMapping(value = "/flight/delete", method = RequestMethod.DELETE)
-    public void deleteFlight(
-            @RequestParam Long id) {
+    @RequestMapping(value = "/flight/update", method = RequestMethod.GET)
+    public String updateFlight(Model model) {
+
+        model.addAttribute("flight", new Flight());
+        return "updateFlight.html";
+    }
+
+    @RequestMapping(value = "/flight/delete/{id}", method = RequestMethod.POST)
+    public String deleteFlight(@PathVariable Long id, Model model) {
 
         flightService.deleteFlight(id);
+
+        List<Flight> flights = repository.findAll();
+        model.addAttribute("flights",flights);
+        return "listFlight.html";
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String listFlight(Model model) {
+
         List<Flight> flights = repository.findAll();
         model.addAttribute("flights", flights);
         return "listFlight.html";
